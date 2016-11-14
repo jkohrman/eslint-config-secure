4426c58ce6bf37b03d9982f46f973079fdfe744d
Jeff Kohrman <jeffrey.kohrman@ldschurch.org>
2016-11-14 16:08:37 -0700

    add config syntax test case

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -18,3 +18,14 @@ test('test properties of config', function (t) {
   t.ok(isObject(config.rules), 'should be truthy - config has rules')
   t.end()
 })
+
+test('load config in eslint to validate rule syntax', function (t) {
+  var CLIEngine = eslint.CLIEngine
+  var cli = new CLIEngine({
+    useEslintrc: false,
+    configFile: 'eslintrc.js'
+  })
+  var code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
+  t.equal(cli.executeOnText(code).errorCount, 0, 'should be truthy - config syntax is valid')
+  t.end()
+})
